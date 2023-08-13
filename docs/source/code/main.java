f3e974d5b9712094283fcd0d090f25edd58bff0d
jh-evans <140018360+jh-evans@users.noreply.github.com>
2023-08-13 22:00:12 +0100

    Create main.java

---
+++ docs/source/code/main.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) {
+    Main m = new Main();
+
+    // When called like this, GETs a webpage as a String
+    Object obj = m.getPage("https://www.example.com");
+
+    if(obj.eval()) {
+        String page = (String) obj.unwrap();
+        System.out.println("The success path");
+    } else {
+        System.out.println("The failure path");
+    }
+}
