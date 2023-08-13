16b2263b31f0fd82673fa870a6ed69417110e718
jh-evans <140018360+jh-evans@users.noreply.github.com>
2023-08-13 23:20:33 +0100

    Create result.java

---
+++ docs/source/code/result.java
@@ -0,0 +1,9 @@
+   private static class Result {
+       public final int status_code;
+       public final String page;
+
+       public Result(int i, String str) {
+           this.status_code = i;
+           this.page = str;
+       }
+   }
