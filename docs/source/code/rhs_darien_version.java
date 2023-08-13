2acafe8197a39a777a1345f5d4d8406b67176b25
jh-evans <140018360+jh-evans@users.noreply.github.com>
2023-08-13 23:35:01 +0100

    Update rhs_darien_version.java

--- docs/source/code/rhs_darien_version.java
+++ docs/source/code/rhs_darien_version.java
@@ -1,1 +1,11 @@
+   private S rhs(String input) {
+       if(FailureUtils.oneIsNull(input)) {
+        	return FailureUtils.theNull(input);
+        }
 
+       if(input.indexOf("-") == -1) {
+         return new FailureValue(-1);
+       } else {
+         return new Success(input.split("-")[1]);
+       }
+   }
