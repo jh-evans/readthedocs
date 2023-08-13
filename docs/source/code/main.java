0c9c7012fac6afb08e9947b515046d96a72183c3
jh-evans <140018360+jh-evans@users.noreply.github.com>
2023-08-13 22:06:40 +0100

    Update main.java

--- docs/source/code/main.java
+++ docs/source/code/main.java
@@ -1,11 +1,12 @@
 public static void main(String[] args) {
     Main m = new Main();
 
-    // When called like this, GETs a webpage as a String
+    // When called like this, GETs a webpage as a String and returns it wrapped inside an ``S`` type
     S obj = m.getPage("https://www.example.com");
 
     if(obj.eval()) {
         String page = (String) obj.unwrap();
+        
         System.out.println("The success path");
     } else {
         System.out.println("The failure path");
