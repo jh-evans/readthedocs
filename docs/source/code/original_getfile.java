ba6d3607564acb7c64c551c5f2ca3c17f10032f2
jh-evans <140018360+jh-evans@users.noreply.github.com>
2023-08-17 23:11:02 +0100

    Update original_getfile.java

--- docs/source/code/original_getfile.java
+++ docs/source/code/original_getfile.java
@@ -1,13 +1,14 @@
-	public String getFile(String filename) throws FileNotFoundException {
-		String line;
-		StringBuilder resultStringBuilder = new StringBuilder();
-	    try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))) {
-	        while ((line = br.readLine()) != null) {
-	            resultStringBuilder.append(line).append("\n");
-	        }
-	    } catch (IOException e) {
-			e.printStackTrace();
-		}
-	
-	    return resultStringBuilder.toString();
-	}
+public String getFile(String filename) throws FileNotFoundException {
+    String line;
+    StringBuilder resultStringBuilder = new StringBuilder();
+
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))) {
+        while ((line = br.readLine()) != null) {
+            resultStringBuilder.append(line).append("\n");
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+    return resultStringBuilder.toString();
+}
