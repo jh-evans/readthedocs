d49cb72da88a49996c5dc4c31eebd73e20adf694
jh-evans <140018360+jh-evans@users.noreply.github.com>
2023-08-18 11:51:05 +0100

    Update original_getfile.java

--- docs/source/code/original_getfile.java
+++ docs/source/code/original_getfile.java
@@ -2,7 +2,7 @@ public String getFile(String filename) throws FileNotFoundException {
     String line;
     StringBuilder resultStringBuilder = new StringBuilder();
 
-    try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))) {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))) { // file opened here
         while ((line = br.readLine()) != null) {
             resultStringBuilder.append(line).append("\n");
         }
